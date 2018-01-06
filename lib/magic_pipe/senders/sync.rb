22f79a2878a449b2508507568db336ae6a03c63a
tom <tommaso@pavese.me>
2018-01-06 15:03:07 +0000

    testing the sync sender

--- lib/magic_pipe/senders/sync.rb
+++ lib/magic_pipe/senders/sync.rb
@@ -4,8 +4,9 @@ module MagicPipe
   module Senders
     class Sync < Base
       def call
-        payload = @codec.new(@data).encode
-        @transport.new(payload, @codec.encoding).submit
+        encoder = @codec.new(@data)
+        payload = encoder.encode
+        @transport.new(payload, encoder.encoding).submit
       end
     end
   end
--- spec/magic_pipe/senders/sync_spec.rb
+++ spec/magic_pipe/senders/sync_spec.rb
@@ -1,4 +1,31 @@
 RSpec.describe MagicPipe::Senders::Sync do
-  it "runs" do
+  let(:data) { double("some ruby object or hash") }
+  let(:codec_k) { double("codec class") }
+  let(:transport_k) { double("transport class") }
+
+  subject do
+    described_class.new(data, codec_k, transport_k)
+  end
+
+  it "has a standard signature to be initialized with three arguments" do
+    expect {
+      subject
+    }.to_not raise_error
+  end
+
+  describe "call" do
+    subject { super().call }
+
+    it "encodes the data with the codec and sends it with the transport" do
+      payload = double("encoded payload")
+      codec = double("codec instance", encode: payload, encoding: :dummy_codec)
+      expect(codec_k).to receive(:new).with(data).and_return(codec)
+
+      transport = double("transport instance")
+      expect(transport_k).to receive(:new).with(payload, :dummy_codec).and_return(transport)
+      expect(transport).to receive(:submit)
+
+      subject
+    end
   end
 end
