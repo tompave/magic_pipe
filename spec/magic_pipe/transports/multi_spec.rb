c79131e94908f6c0c5f8118c13318c4ca3b22b09
tom <tommaso@pavese.me>
2018-01-24 13:00:20 +0000

    Fix webmock error on travis

--- spec/magic_pipe/transports/multi_spec.rb
+++ spec/magic_pipe/transports/multi_spec.rb
@@ -31,31 +31,39 @@ RSpec.describe MagicPipe::Transports::Multi do
       }
     end
 
+    let(:fake_https) { instance_double(MagicPipe::Transports::Https) }
+    let(:fake_sqs) { instance_double(MagicPipe::Transports::Sqs) }
+
+    before do
+      allow(MagicPipe::Transports::Https).to receive(:new).with(config, metrics).and_return(fake_https)
+      allow(MagicPipe::Transports::Sqs).to receive(:new).with(config, metrics).and_return(fake_sqs)
+    end
+
     def perform
       subject.submit(payload, metadata)
     end
 
     it "forwards the payload to all the nested transports" do
-      expect_any_instance_of(MagicPipe::Transports::Https).to receive(:submit).with(payload, metadata)
-      expect_any_instance_of(MagicPipe::Transports::Sqs).to receive(:submit).with(payload, metadata)
+      expect(fake_https).to receive(:submit).with(payload, metadata)
+      expect(fake_sqs).to receive(:submit).with(payload, metadata)
       perform
     end
 
     describe "if one of the transports raises an error" do
       before do
-        expect_any_instance_of(MagicPipe::Transports::Https).to receive(:submit) do
+        expect(fake_https).to receive(:submit) do
           raise "oh no"
         end
       end
 
       it "logs the error and doesn't halt the pipeline" do
-        expect_any_instance_of(MagicPipe::Transports::Sqs).to receive(:submit).with(payload, metadata)
+        expect(fake_sqs).to receive(:submit).with(payload, metadata)
 
-        expect {
-          perform
-        }.to output(
-          %r{Transports::Multi, error submitting with MagicPipe::Transports::Https}
-        ).to_stdout
+        expect(config.logger).to receive(:error).with(
+          %r{Transports::Multi, error submitting with}
+        )
+
+        perform
       end
     end
   end
